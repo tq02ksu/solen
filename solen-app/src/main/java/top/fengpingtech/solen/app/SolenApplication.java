6fe5d0e8178a9affe536e00a625452d15841a3a6
dongbowu <dongbowu@neusoft.com>
2022-06-29 23:42:37 +0800

    add cors configuration

--- solen-app/src/main/java/top/fengpingtech/solen/app/SolenApplication.java
+++ solen-app/src/main/java/top/fengpingtech/solen/app/SolenApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import top.fengpingtech.solen.app.config.AuthProperties;
 import top.fengpingtech.solen.app.config.SolenServerProperties;
 import top.fengpingtech.solen.app.service.SolenServerStarter;
@@ -19,5 +22,15 @@ public class SolenApplication {
     @Bean
     DeviceService deviceService(SolenServerStarter starter) {
         return starter.getDeviceService();
+    }
+
+    @Bean
+    WebMvcConfigurer webConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
